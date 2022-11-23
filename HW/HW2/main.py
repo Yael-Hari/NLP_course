226622b7e6ce74eca47f208ddb1576267c7c8248
YaelHari <yaelhhari@gmail.com>
2022-11-23 19:05:09 +0200

    preprocessing data not comleted

--- HW/HW2/main.py
+++ HW/HW2/main.py
@@ -5,8 +5,11 @@ from gensim import downloader
 import torch
 
 
+VEC_DIM = 25
+GLOVE_PATH = f'glove-twitter-{VEC_DIM}'  # TODO change to 200
+
+
 def load_glove():
-    GLOVE_PATH = 'glove-twitter-25'   # TODO change to 200
     glove = downloader.load(GLOVE_PATH)
     # glove.key_to_index
     return glove
@@ -29,22 +32,37 @@ def preprocess_data():
     # split word from tag
     word_tag_tuples_list = []
     for sentence in sentences:
-        word_tag_tuples = [("*", "*"), ("*", "*")] + [tuple(s.split('\t')) for s in sentence] + [("~", "~"), ("~", "~")]
+        word_tag_tuples = [("*", "*"), ("*", "*")] + \
+                          [tuple(s.split('\t')) for s in sentence] + \
+                          [("~", "~"), ("~", "~")]
         word_tag_tuples_list.append(word_tag_tuples)
 
     # glove_dict:
-    glove_dict = load_glove()
+    glove = load_glove()
 
     # concat every word vec to 2 words behind and 2 after
     # "*", "~" are 0 vecs for now. TODO: something else?
     # also, if a word not found in glove_dict, we put 0 vec. TODO: maybe there is a better idea?
+    # TODO: if selected vector is not in glove: for now we give it 0
     X = []
     y = []
     for sentence in word_tag_tuples_list:
-        for i_s, (s_word, s_tag) in enumerate(sentence[2:-2]):
+        sentence_len = len(sentence)
+        for i_s in range(2, sentence_len-2):
+            s_word, s_tag = sentence[i_s]
             vecs_list = []
-            for c_word, c_tag in sentence[i_s-2: i_s+2]:
-                pass
+            for c_word, c_tag in sentence[i_s-2: i_s+3]:
+                if (c_word == "*") or (c_word == "~") or (c_word not in glove.key_to_index):
+                    u_c = np.zeros(VEC_DIM)
+                else:
+                    u_c = glove[c_word]
+                vecs_list.append(u_c)
+            concated_vec = np.concatenate(vecs_list)
+            X.append(concated_vec)
+            y.append(s_tag)
+
+
+
 
 
 def main():
