7d39bebe38f77b4742172788be0a969c10fde9e8
yael <yael.hari@mktmediastats.com>
2023-01-02 04:16:53 -0500

    made the mini_mini_train.labeled file

--- HW/HW3/preprocess.py
+++ HW/HW3/preprocess.py
@@ -66,7 +66,8 @@ class SentencesEmbeddingDataset:
         self.comp_path = "comp.unlabeled"
 
         # FOR DEBUG
-        self.train_path = "mini_train.labeled"
+        self.train_path = "mini_mini_train.labeled"
+        self.val_path = "mini_mini_train.labeled"
 
     def get_pos_embeddings(self, pos_values):
         if self.pos_embedding_name == "learn":
