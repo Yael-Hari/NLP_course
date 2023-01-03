cf8cafc191779d3ba8a8bd563ffeb1065e6a1867
keren_g <keren.gruteke@wiliot.com>
2023-01-03 18:08:32 +0200

    optimizer name

--- HW/HW3/generate_comp_tagged.py
+++ HW/HW3/generate_comp_tagged.py
@@ -20,7 +20,7 @@ def main():
     lstm_num_layers = 1
     lstm_dropout = 0
     activation = nn.Tanh()
-    optimizer = "ADAM"
+    optimizer_name = "ADAM"
 
     torch.manual_seed(42)
     load_dataset_from_pkl = False
@@ -60,14 +60,14 @@ def main():
     hyper_params_title += f" | hidden={lstm_hidden_dim}"
     hyper_params_title += f" \nnum_layers={lstm_num_layers}"
     hyper_params_title += f" | dropout={lstm_dropout}"
-    hyper_params_title += f" | opt={optimizer}"
+    hyper_params_title += f" | opt={optimizer_name}"
     model_name = "mini_train3 | "
     model_name += f"word_embedding_name={word_embedding_name}"
     model_name += f" | pos={pos_embedding_name}"
     model_name += f" | hidden={lstm_hidden_dim}"
     model_name += f" | num_layers={lstm_num_layers}"
     model_name += f" | dropout={lstm_dropout}"
-    model_name += f" | opt={optimizer}"
+    model_name += f" | opt={optimizer_name}"
     print(hyper_params_title)
     model_save_path = f"{model_name}.pt"
 
