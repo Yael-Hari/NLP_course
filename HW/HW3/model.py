88e98cf79ae1c30979e16b3844efe0c61c76b41c
keren_g <keren.gruteke@wiliot.com>
2023-01-03 16:24:41 +0200

    model name

--- HW/HW3/model.py
+++ HW/HW3/model.py
@@ -199,13 +199,20 @@ def main():
                         )
 
                         hyper_params_title = (
-                            f"{word_embedding_name=} | {pos_embedding_name=}"
+                            f"word_embedding_name={word_embedding_name}"
                         )
+                        hyper_params_title += f" | pos={pos_embedding_name}"
                         hyper_params_title += f" | hidden={lstm_hidden_dim}"
                         hyper_params_title += f" \nnum_layers={lstm_num_layers}"
                         hyper_params_title += f" | dropout={lstm_dropout}"
+                        model_name = "mini_train3 | "
+                        model_name += f"word_embedding_name={word_embedding_name}"
+                        model_name += f" | pos={model_name}"
+                        model_name += f" | hidden={lstm_hidden_dim}"
+                        model_name += f" | num_layers={lstm_num_layers}"
+                        model_name += f" | dropout={lstm_dropout}"
                         print(hyper_params_title)
-                        model_save_path = f"{hyper_params_title}.pt"
+                        model_save_path = f"{model_name}.pt"
 
                         dependency_model = DependencyParser(
                             embedding_dim=word_embedding_dim
