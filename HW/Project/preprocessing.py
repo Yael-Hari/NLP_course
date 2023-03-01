8951e8c4c706a9bdf957b48787f59a45b126c8c5
yael <yael.hari@mktmediastats.com>
2023-03-01 07:22:09 -0500

    made changes for bert

--- HW/Project/preprocessing.py
+++ HW/Project/preprocessing.py
@@ -1,5 +1,4 @@
 import torch
-from torch.nn import functional as F
 from torch.utils.data import DataLoader
 
 from transformers import AutoModel, AutoTokenizer
@@ -8,25 +7,61 @@ from project_evaluate import read_file
 
 class DeEnPairsData:
     def __init__(self):
-        self.tokenizer = AutoTokenizer.from_pretrained("jhu-clsp/bibert-ende")  # TODO change to german bert
-        self.bibert_model = AutoModel.from_pretrained("jhu-clsp/bibert-ende")   # TODO change to german bert
+        self.model_name   = "bert-base-german-cased"
+        self.tokenizer    = AutoTokenizer.from_pretrained(self.model_name)  # tokenizer.decode(["input_ids"])
+        self.bert_model   = AutoModel.from_pretrained(self.model_name)
 
         self.train_path   = 'data/train.labeled'
         self.val_path     = 'data/val.labeled'
 
-    def get_embedding_data_loader_labeled(self, file_path):
+        # ~~~~~~~~~~~~~~~~ for DEBUG ~~~~~~~~~~~~~~~~
+        self.train_path   = 'data/mini_train.labeled'
+        self.val_path     = 'data/mini_val.labeled'
+
+    # ~~~~~~~~~~~~~~~~~ FINE TUNE BERT
+
+    def get_labeled_loaders(self):
+
+        X_train, y_train = self.get_labeled_data_bert(self.train_path)
+        X_val, y_val = self.get_labeled_data_bert(self.val_path)
+
+        # create datasets
+        train_dataset = [*zip(X_train, y_train)]
+        val_dataset = [*zip(X_val, y_val)]
+
+        # create dataloader
+        train_loader = DataLoader(
+            train_dataset, batch_size=1, shuffle=False
+        )
+        val_loader = DataLoader(
+            val_dataset, batch_size=1, shuffle=False
+        )
+
+        return train_loader, val_loader
+
+    def get_labeled_data_bert(self, file_path):
+
+        # read data files
         file_en, file_de = read_file(file_path)
 
-        de_bibert_embedding = []
+        de_bert_embedding = []
         for de_str in file_de:
+            # Transform input to tokens
             tokens = self.tokenizer.convert_tokens_to_ids(self.tokenizer.tokenize(de_str))
-            input_ids = torch.tensor(tokens, dtype=torch.long).unsqueeze(0)
+            input_ids = torch.tensor(tokens, dtype=torch.long)  #.unsqueeze(0)
 
-            # Obtain the contextual embeddings of BiBERT
+            # Obtain the contextual embeddings of BERT
             layer = -1  # Last layer
-            de_bibert_emb = self.bibert_model(input_ids, output_hidden_states=True)[2][layer]
-            de_bibert_embedding.append(de_bibert_emb)
+            de_bert_emb = self.bert_model(input_ids, output_hidden_states=True)[2][layer]
+            de_bert_embedding.append(de_bert_emb)
+
+            # Transform input tokens
+            inputs = self.tokenizer("Hello world!", return_tensors="pt")
+
+            # Model apply
+            outputs = self.bert_model(**inputs)
         # TODO
+        print()
 
     def get_tokens_labeled_data_loaders(self):
         X_train, y_train = self.get_tokens_labeled(self.train_path)
@@ -68,6 +103,7 @@ class DeEnPairsData:
 
 
 
+
 if __name__ == '__main__':
     dataset = DeEnPairsData()
-    dataset.get_embedding_data_loader_labeled(dataset.train_path)
+    train_loader, val_loader = dataset.get_labeled_loaders()
